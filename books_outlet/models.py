e830828a76edc7f17eb7de00d1dd7aee75cb0e00
anwar-2000 <khaledkhaouani5@gmail.com>
2023-02-05 21:31:48 +0000

    Refresher 1.1 'tables relations'

--- books_outlet/models.py
+++ books_outlet/models.py
@@ -1,13 +1,23 @@
 from django.db import models
 from django.core.validators import MinValueValidator , MaxValueValidator
 from django.urls import reverse
+from django.utils.text import slugify
 # Create your models here.
+class Author(models.Model):
+    firstName = models.CharField(max_length=50)
+    lastName = models.CharField(max_length=50)
 
 class Book(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(10)])
-    author = models.CharField(null=True , max_length=100)
+    author = models.ForeignKey(Author,on_delete=models.CASCADE,null=True)
     isBestSelling = models.BooleanField(default=False)
+    slug = models.SlugField(default="", null=False)
+
+    def save(self ,*args, **kwargs):
+        #calling this method will trigger cthis line then the actuall save method
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
 
     def get__absolute__url(self):
         return reverse('book-detail', args=[self.id])
